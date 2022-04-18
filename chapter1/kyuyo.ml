db1f19a5edfdfcbf849d73b6f75d7592bc2f9660
Kaikei-e <e_kaikei@protonmail.com>
2022-04-19 00:35:54 +0900

    Update: finished chapter1.

--- chapter1/kyuyo.ml
+++ chapter1/kyuyo.ml
@@ -1,8 +1,11 @@
-(* kyuyo : int -> int * *)
-let kyuyo x = 0
+let jikyu = 950
+let kihonkyu = 100
 
+
+(* kyuyo : int -> int * *)
+let kyuyo x = kihonkyu + x*jikyu
 (* Test *)
-let test1 = kyuyo * 25 = 23580
-let test2 = kyuyo * 28 = 26700
-let test3 = kyuyo * 31 = 29550
+let test1 = kyuyo 25 = 23850
+let test2 = kyuyo 28 = 26700
+let test3 = kyuyo 31 = 29550
 
